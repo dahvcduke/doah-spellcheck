7d77b1d711ead10d1eb09ac28d739d7f8a48875a
bonche22 <bonnie.chen1012@gmail.com>
2025-04-12 23:31:27 -0400

    Updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
-import DataStoragePage from "./pages/Data/DataStoragePage";
-import AllDocumentsPage from "./pages/Data/AllDocumentsPage";
-import EditingHistoryPage from "./pages/Data/EditingHistoryPage";
-import ChooseDocumentPage from "./pages/Edit/ChooseDocumentPage";
-import ErrorProcessPage from "./pages/Edit/ErrorProcessPage";
-import EditErrorPage from "./pages/Edit/EditErrorPage";
-import AllErrorCorrectedPage from "./pages/Edit/AllErrorCorrectedPage";
+import ChooseDocumentPage from "./pages/ChooseDocumentPage";
+import ErrorProcessPage from "./pages//ErrorProcessPage";
+import EditErrorPage from "./pages/EditErrorPage";
+import AllErrorCorrectedPage from "./pages/AllErrorCorrectedPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/data-storage" element={<DataStoragePage />} />
-        <Route path="/all-documents" element={<AllDocumentsPage />} />
-        <Route path="/editing-history" element={<EditingHistoryPage />} />
         <Route path="/choose-document" element={<ChooseDocumentPage />} />
         <Route path="/error-process" element={<ErrorProcessPage />} />
         <Route path="/edit-error" element={<EditErrorPage />} />
